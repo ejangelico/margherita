4ce31991c082f5a03aec9f934a9d7808bb55ef6c
Evan Angelico <ejangelico@uchicago.edu>
2016-05-05 09:41:18 -0500

    v4 Current working version

---
+++ C3PO/user/rtm.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# read a temperature
+# usage rtm <SP no 1..6> <value>
+# HA level 1
+
+import sys
+import subprocess
+#import subprocess32 # more recent, for POSIX
+import psycopg2  # needed to access database to get path to HA Level 0
+import re
+
+f = open('./userinfo','r')
+unixuser = f.readline()
+unixuser = re.sub('\n','',unixuser)
+dbase    = f.readline()
+dbase    = re.sub('\n','',dbase)
+
+# get path to level 0
+try: conn = psycopg2.connect("dbname='"+dbase+"' user='"+unixuser+"' ")
+except: raise Exception("Can't connect to database "+dbase+" as user "+unixuser)
+cur = conn.cursor()
+try: cur.execute("SELECT value FROM admin WHERE key = 'HSpath'")
+except: raise Exception("Can\'t find level-0 path in table admin")
+
+rows = cur.fetchall()
+if len(rows) == 1:
+  cmdpath = rows[0][0]
+#    
+if len(sys.argv) <> 2:
+  raise Exception('rtm needs 1 argument')
+  
+TMno = "TM"+sys.argv[1]
+# TMx is a PV alias (look into file aliasdefs or database table "alias")
+# call pvw: write to PV
+#cmd = cmdpath+"pvw.py ssp.py "+SPno+" "+value
+#print cmd
+subprocess.call([cmdpath+"pvr.py","rtm.py",TMno])
+#                                  ^ 
+#                                  traceback
+
+
