a46408ce2ee8568ce83b3340b547da7d3867f84e
ejangelico <ejangelico@uchicago.edu>
2016-06-21 17:30:14 -0500

    Almost working version of controlLogger

--- margherita_run_scripts/ControlZone.py
+++ margherita_run_scripts/ControlZone.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
-
+import os
+import sys
+import subprocess
 
 #class representing a single zone of the temperature controller
-class ControlZone():
-	def __init__(self, z = 0, e = 0, s = 25, h = 10, l = 10):
+class ControlZone:
+	def __init__(self, z = 0, e = False, s = 25, h = 10, l = 10):
 		self.zone = z 				#zone number
 		self.enable = e 			#boolean, enable zone = 1, disable zone = 0
 		self.setpoint = s 			#temperature setpoint for zone when enabled
 		self.ha = h 				#alarm at "high alarm", 'ha' degrees above setpoint
 		self.la = l 				#alarm at "low alarm", 'la' degrees below setpoint
-		self.changed = 0			#marks if the zone has recently had a changed parameter, 0 for false 1 for true
+		self.changed = False		#marks if the zone has recently had a changed parameter, 0 for false 1 for true
 
 	def printZone(self):
 		print "-----------------------"
@@ -20,11 +22,24 @@ class ControlZone():
 		print "- low alarm: " + str(self.la) + " -"
 		print "-----------------------"
 
-	def setEnable(self, e):
-		if(e != 0 or e != 1): 
-			print "Please give the enable either 1 or 0"
-			return
+	def __str__(self):
+		output = "--------------------\n"
+		output += "- zone: " + str(self.zone) + " -\n"
+		output += "- enabled: " + str(self.enable) + " -\n"
+		output += "- setpoint: " + str(self.setpoint) + " -\n"
+		output += "- high alarm: " + str(self.ha) + " -\n"
+		output += "- low alarm: " + str(self.la) + " -\n"
+		output += "-----------------------\n"
+		return output
 
+	def sendParameters(self):
+		print "SENT NEW PARAMETERS on zone " + str(self.zone)
+		self.changed = False
+
+	def setZone(self, z):
+		self.zone = z
+
+	def setEnable(self, e):
 		self.enable = e
 
 	def setSetpoint(self, s):
@@ -32,16 +47,38 @@ class ControlZone():
 		self.setpoint = s
 
 	def setHA(self, h):
-		if(h <= self.setpoint): 
-			print "Error: trying to set the high alarm less than or equal to the setpoint"
-			return
 		self.ha = h
 
 	def setLA(self, l):
-		if(l >= self.setpoint):
-			print "Error: trying to set the low alarm greater than or equal to the setpoint"
-			return
 		self.la = l
+
+	def setChanged(self, c):
+		self.changed = c
+
+	def getZone(self):
+		return self.zone
+
+	def getEnable(self):
+		return self.enable
+
+	def getSetpoint(self):
+		return self.setpoint
+
+	def getHA(self):
+		return self.ha
+
+	def getLA(self):
+		return self.la
+
+	def hasChanged(self):
+		return self.changed
+
+	def getZoneTemp(self):
+		process = subprocess.Popen(['./rtm.py', str(chNum)], stdout=subprocess.PIPE)
+		out, err = process.communicate()
+		outfile.write(str(out.split('\n')[0]))
+
+
 
 
 	
--- margherita_run_scripts/controlLogger.py
+++ margherita_run_scripts/controlLogger.py
@@ -1,47 +1,122 @@
 #!/usr/bin/python
 import time
 import os
+import sys
 import numpy as np
 import ControlZone
 
 
-
-def updatePoints(modtime):
-	print "File was modified at " + str(time.ctime(modtime))
-	return 
-
 #sets the zone parameters for each zone in the controller set
 def setAllZoneParameters(setfile, zoneArray):
-	lineStructure = np.genfromtxt(setfile, delimiter=)
+	lineStructure = np.genfromtxt(setfile, delimiter=',')
+	if len(lineStructure) != len(zoneArray):
+		print "ERROR: Number of zones/lines in parameter file has changed or is incorrect!"
+		return
+
+	#go through each line/zone and set the parameters
+	#for each parameter, it checks if it is making any "changes"
+	#if it is not making a change, it passes by, if it does, it makes the "change" flag
+	for i in range(len(zoneArray)):
+		if(int(lineStructure[i][0]) != zoneArray[i].getZone()):
+			zoneArray[i].setZone(int(lineStructure[i][0]))
+			zoneArray[i].setChanged(True)
+
+		if(bool(lineStructure[i][1]) != zoneArray[i].getEnable()):
+			zoneArray[i].setEnable(bool(lineStructure[i][1]))
+			zoneArray[i].setChanged(True)
+			
+		if(int(lineStructure[i][2]) != zoneArray[i].getSetpoint()):
+			zoneArray[i].setSetpoint(int(lineStructure[i][2]))
+			zoneArray[i].setChanged(True)
+
+		if(int(lineStructure[i][3]) != zoneArray[i].getHA()):
+			zoneArray[i].setHA(int(lineStructure[i][3]))
+			zoneArray[i].setChanged(True)
+
+		if(int(lineStructure[i][4]) != zoneArray[i].getLA()):
+			zoneArray[i].setLA(int(lineStructure[i][4]))
+			zoneArray[i].setChanged(True)	
+
+def sendModifiedParameters(zoneArray):
+	#look at each zone and see if they have changed
+	for i in range(len(zoneArray)):
+		if(zoneArray[i].hasChanged()):
+			zoneArray[i].sendParameters()
+
+
+
+
+def logTemperatures(templogfile):
+	outfile = open(templogfile,'a')
+	output = []
+	for x in zoneArray:
+		output.append(x.getZoneTemp())
+
+
+	print "Logged temps..."
+
+
+
+def saveSetfileSnapshot(snapshotfile, zoneArray):
+	try:
+		outfile = open(snapshotfile, "a")
+	except IOError:
+		print "Could not open snapshot file"
+		return
+
+	outfile.write("********" + time.strftime("%y:%m:%d %H:%M:%S") + "*********")
+	for x in zoneArray:
+		outfile.write(str(x))
+
+	outfile.close()
 
 	
 
-#(mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(setfile)
-#lastmod =  mtime
+
 
 if __name__ == '__main__':
-	setfile = "setpoints.txt"
+
+	setfile = "setpoints.txt"		#file to look in for setpoint configurations
+	snapshotfile = "snapshots.txt" 	#file to save each change in the setpoint configuration
+	templogfile = "controlrun0.txt" #file to log control TC's into
+
+	#get the initial timestamp for file modification on the setfile
+	(mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(setfile)
+	lastmod =  mtime
 
 	#initialize an array of zones based on how many lines are in the set file
 	with open(setfile) as f:
 		for i, l in enumerate(f):
 			pass
 	NZones = i + 1
-	zoneArray = [ControlZone for _ in range(NZones)]
+	zoneArray = [ControlZone.ControlZone() for _ in range(NZones)]
 
 
 	#load the setfile for the first time
 	#set all zone parameters for the first time
 	setAllZoneParameters(setfile, zoneArray)
 
-'''
-while True:
-	print "Checking for modification"
-	(mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(setfile)
-	if (lastmod != mtime):
-		updatePoints(mtime)
-		lastmod = mtime
+	#save a snapshot of the first configurations
+	saveSetfileSnapshot(snapshotfile,zoneArray)
 
-	time.sleep(1)
-'''
+	#send these initial parameters
+	sendModifiedParameters(zoneArray)
+
+	
+	#start logging and watching
+	while True:
+		#check if the setfile has been modified
+		(mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(setfile)
+		if (lastmod != mtime):
+			setAllZoneParameters(setfile,zoneArray)
+			saveSetfileSnapshot(snapshotfile, zoneArray)
+			sendModifiedParameters(zoneArray)
+			lastmod = mtime
+
+		logTemperatures(templogfile)
+		time.sleep(3)
+
+	
+
+
 	
